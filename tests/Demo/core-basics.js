b02978278609e8dcdc4b307fb8a21e2bf29fe39a
rakesh.ranjan.qaengineer@gmail.com <rakesh.ranjan.qaengineer@gmail.com>
2025-12-25 07:48:59 +0530

    first commit Dec 2025

---
+++ tests/Demo/core-basics.js
@@ -0,0 +1,16 @@
+let age = 35;
+age = 26;
+console.log('Age is:', age);
+
+const name = 'Rakesh';
+country = 'India';
+console.log('Name is:', name);
+console.log('Country is:', country);
+
+let firstname = 'Rakesh';
+let lastname = 'Ranjan';
+console.log('Full Name is:', firstname + ' ' + lastname);
+console.log("Length of the String is:", firstname.length)
+console.log("Hello", firstname, lastname)
+console.log(firstname.toLowerCase());
+console.log(lastname.toUpperCase());
---
+++ tests/Demo/hello.js
@@ -0,0 +1,7 @@
+exports.hello = function f1() {
+    return "Hello, World!";
+}
+
+exports.helloworld = function f2() {
+    return "Hello, Playwright!";
+}
